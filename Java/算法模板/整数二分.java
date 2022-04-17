49472b0fadc0247272c2d70620f5e121910ce30c
phoenix044 <wang1209829055@163.com>
2022-04-17 18:32:16 +0800

    二分查找算法模板

---
+++ Java/算法模板/整数二分.java
@@ -0,0 +1,21 @@
+public static boolean check(int x){
+    return false;
+} 
+
+public static int bsearch_1(int[] q, int l, int r){
+    while(l < r){
+        int mid = l + r >> 1;
+        if check(mid) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
+
+public static int bsearch_2(int[] q, int l, int r){
+    while(l < r){
+        int mid = l + r + 1 >> 1;
+        if check(mid) l = mid;
+        else r = mid - 1;
+    }
+    return l;
+}
